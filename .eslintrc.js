9b8a8ca85198f7978985e4fb45877e85d0f9de33
stagas <gstagas@gmail.com>
2021-12-22 18:27:35 +0200

    chore: housekeeping

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,33 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true,
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
   ],
   ignorePatterns: ['dist', 'node_modules'],
   plugins: ['import'],
   settings: {
     react: {
       pragma: 'h',
-      fragment: 'Fragment'
-    }
+      fragment: 'Fragment',
+    },
   },
   rules: {
     '@typescript-eslint/no-extra-semi': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
-      { args: 'all', argsIgnorePattern: '^_' }
-    ]
-  }
+      { args: 'all', argsIgnorePattern: '^_' },
+    ],
+  },
 }
